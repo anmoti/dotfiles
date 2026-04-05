e74f1734f84dd2aeb8d9b432d6350901077e71ed
anmoti <77442643+anmoti@users.noreply.github.com>
2026-04-06 07:40:14 +0900

    refactor(neovim): LSPのキーマップをLspAttachのときバッファローカルで作成

--- dot_config/nvim/lua/configs/keymaps.lua
+++ dot_config/nvim/lua/configs/keymaps.lua
@@ -1,7 +1,12 @@
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {
-  desc = "Hover",
-})
-
-vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, {
-  desc = "LSP Code Action",
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {
+      buffer = args.buf,
+      desc = "Hover",
+    })
+    vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, {
+      buffer = args.buf,
+      desc = "LSP Code Action",
+    })
+  end,
 })
